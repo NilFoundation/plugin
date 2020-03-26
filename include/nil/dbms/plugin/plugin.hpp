48e5e7c68264834c6c3d9f4e708a9d3103d23f78
Mikhail Komarov <nemo@nil.foundation>
2020-03-26 16:59:42 +0300

    Plugin interface updated.

--- include/nil/dbms/plugin/plugin.hpp
+++ include/nil/dbms/plugin/plugin.hpp
@@ -15,8 +15,8 @@
 // <https://github.com/NilFoundation/plugin/blob/master/LICENSE_1_0.txt>.
 //----------------------------------------------------------------------------
 
-#ifndef DBMS_PLUGIN_HPP
-#define DBMS_PLUGIN_HPP
+#ifndef DBMS_BASIC_PLUGIN_HPP
+#define DBMS_BASIC_PLUGIN_HPP
 
 #include <boost/config.hpp>
 #include <boost/program_options/options_description.hpp>
@@ -24,29 +24,31 @@
 
 namespace nil {
     namespace dbms {
-        /*!
-         * @brief
-         */
-        class BOOST_SYMBOL_VISIBLE plugin {
-            enum state {
-                registered,     ///< the plugin is constructed but doesn't do anything
-                initialized,    ///< the plugin has initialized any state required but is idle
-                started,        ///< the plugin is actively running
-                stopped         ///< the plugin is no longer running
-            };
+        namespace plugin {
+            /*!
+             * @brief
+             */
+            struct BOOST_SYMBOL_VISIBLE basic {
+                enum state {
+                    registered,     ///< the plugin is constructed but doesn't do anything
+                    initialized,    ///< the plugin has initialized any state required but is idle
+                    started,        ///< the plugin is actively running
+                    stopped         ///< the plugin is no longer running
+                };
 
-            virtual ~plugin() {
-            }
-            virtual state get_state() const = 0;
-            virtual const std::string &name() const = 0;
-            virtual void set_program_options(boost::program_options::options_description &cli,
-                                             boost::program_options::options_description &cfg) = 0;
-            virtual void initialize(const boost::program_options::variables_map &options) = 0;
-            virtual void handle_sighup() = 0;
-            virtual void startup() = 0;
-            virtual void shutdown() = 0;
-        };
-    }    // namespace dbms
+                virtual ~basic() {
+                }
+                virtual state get_state() const = 0;
+                virtual const std::string &name() const = 0;
+                virtual void set_program_options(boost::program_options::options_description &cli,
+                                                 boost::program_options::options_description &cfg) = 0;
+                virtual void initialize(const boost::program_options::variables_map &options) = 0;
+                virtual void handle_sighup() = 0;
+                virtual void startup() = 0;
+                virtual void shutdown() = 0;
+            };
+        }    // namespace plugin
+    }        // namespace dbms
 }    // namespace nil
 
 #endif    // DBMS_PLUGIN_HPP
